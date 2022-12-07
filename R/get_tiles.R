a525cad90c8a98c6db2af396d2c4ce9128c1b8a6
Michael Mahoney <mike.mahoney.218@gmail.com>
2022-12-07 13:30:02 +0000

    version 0.7.2

--- R/get_tiles.R
+++ R/get_tiles.R
@@ -41,8 +41,10 @@
 #'
 #' * [3DEPElevation](https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer)
 #'   (short code: elevation)
-#' * [USGSNAIPPlus](https://services.nationalmap.gov/arcgis/rest/services/USGSNAIPPlus/MapServer)
+#' * [USGSNAIPPlus](https://imagery.nationalmap.gov/arcgis/rest/services/USGSNAIPPlus/ImageServer/exportImage)
 #'   (short code: ortho)
+#' * [HRO](https://imagery.nationalmap.gov/arcgis/rest/services/HRO/ImageServer)
+#' * [USGSNAIPImagery](https://imagery.nationalmap.gov/arcgis/rest/services/USGSNAIPImagery/ImageServer)
 #' * [nhd](https://hydro.nationalmap.gov/arcgis/rest/services/nhd/MapServer)
 #'   (short code: hydro)
 #' * [govunits](https://carto.nationalmap.gov/arcgis/rest/services/govunits/MapServer)
